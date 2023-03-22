b9f2ec2c9bc0e05f1c138caf695219cbb51f56ed
hayeonlee12 <54343481+hayeonlee12@users.noreply.github.com>
2023-03-22 16:20:16 +0900

    moved java file

---
+++ Practice/workspace/gitjava/src/javagit/Practice01.java
@@ -0,0 +1,19 @@
+package javagit;
+
+public class Practice01 {
+
+	public static void main(String[] args) {
+		String name;
+		int age;
+		String intro;
+		
+		name = "학생";
+		age = 13;
+		intro = "잘 부탁드립니다.";
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(intro);
+	}
+
+}
